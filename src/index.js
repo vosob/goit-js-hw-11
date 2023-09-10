5a14c71f667acf26aadf57c70a0b8d80389d7a6a
vosob <osobskyi.vlad@gmail.com>
2023-09-10 21:31:22 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,52 +1,17 @@
-import axios from 'axios';
-import queryString from 'query-string';
 import Notiflix from 'notiflix';
-
-const PER_PAGE = 40;
+import { total, isLoading, loadData, PER_PAGE } from './js/api';
+import { renderCards, appeandCards, refs as renderRefs } from './js/render';
 
 let page = 1;
 let q = '';
-let total = 0;
-let isLoading = false;
+
 let isLastFetched = false;
 
 const refs = {
   form: document.getElementById('search-form'),
   searchInput: document.querySelector('form input'),
-  gallery: document.querySelector('.gallery'),
+  gallery: renderRefs.gallery,
   loadMoreButton: document.querySelector('.load-more'),
-};
-
-const getCard = ({ webformatURL, likes, views, comments, downloads }) => {
-  return `
-    <div class="photo-card">
-      <img src="${webformatURL}" alt="" loading="lazy" />
-        <div class="info">
-          <p class="info-item">
-              <b>Likes: ${likes}</b>
-          </p>
-          <p class="info-item">
-              <b>Views: ${views}</b>
-          </p>
-          <p class="info-item">
-              <b>Comments: ${comments}</b>
-          </p>
-          <p class="info-item">
-              <b>Downloads: ${downloads}</b>
-          </p>
-      </div>
-    </div>
-    `;
-};
-
-const renderCards = hits => {
-  const html = hits.map(getCard).join('');
-  refs.gallery.innerHTML = html;
-};
-
-const appeandCards = hits => {
-  const html = hits.map(getCard).join('');
-  refs.gallery.innerHTML += html;
 };
 
 // const showLoadMore = () => {
@@ -66,65 +31,29 @@ const scrollIntoHead = pos => {
   }, 0);
 };
 
-const loadData = (q, page) => {
-  if (!q)
-    throw new Erorr("Помилка: параметр 'q' не визначено або має значення null");
-  isLoading = true;
-
-  const url = queryString.stringifyUrl({
-    url: 'https://pixabay.com/api/',
-    query: {
-      q,
-      page,
-      key: '39347211-5080f64175fc5e961a62f5c8b',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      per_page: PER_PAGE,
-    },
-  });
-
-  return axios
-    .get(url)
-    .then(({ data }) => {
-      total += data.hits.length;
-      if (data.totalHits === 0) {
-        Notiflix.Notify.warning(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      return data;
-    })
-    .catch(error => {
-      console.log('error: ', error);
-      return null;
-    })
-    .finally(() => {
-      isLoading = false;
-    });
-};
-
-const loadMore = () => {
+const loadMore = async () => {
   page += 1;
 
   const currenLast = total;
 
-  loadData(q, page).then(({ totalHits, hits }) => {
-    scrollIntoHead(currenLast + 1);
+  const response = await loadData(q, page);
+  if (response === null) {
+    return;
+  }
+  scrollIntoHead(currenLast + 1);
 
-    appeandCards(hits);
-    console.log({ total, totalHits });
-    if (total >= totalHits) {
-      // hideLoadMore();
-      isLastFetched = true;
-      Notiflix.Notify.warning(
-        `We're sorry, but you've reached the end of search results.`
-      );
-    }
-  });
+  appeandCards(response.hits);
+
+  if (total >= response.totalHits) {
+    // hideLoadMore();
+    isLastFetched = true;
+    Notiflix.Notify.warning(
+      `We're sorry, but you've reached the end of search results.`
+    );
+  }
 };
 
-const submitHandler = e => {
+const submitHandler = async e => {
   e.preventDefault();
   q = refs.searchInput.value;
 
@@ -133,18 +62,22 @@ const submitHandler = e => {
   total = 0;
   page = 1;
   isLastFetched = false;
-  // hideLoadMore();
-  loadData(q, page).then(({ totalHits, hits }) => {
-    renderCards(hits);
-    if (totalHits) {
-      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-    }
 
-    isLastFetched = totalHits < PER_PAGE;
-    // if (totalHits > PER_PAGE) {
-    //   showLoadMore();
-    // }
-  });
+  // hideLoadMore();
+
+  const response = await loadData(q, page);
+  if (response === null) {
+    return;
+  }
+  renderCards(response.hits);
+  if (response.totalHits) {
+    Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
+  }
+
+  isLastFetched = response.totalHits < PER_PAGE;
+  // if (totalHits > PER_PAGE) {
+  //   showLoadMore();
+  // }
 };
 
 refs.form.onsubmit = submitHandler;
---
+++ src/js/api.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import queryString from 'query-string';
+import Notiflix from 'notiflix';
+
+export let total = 0;
+export let isLoading = false;
+export const PER_PAGE = 40;
+
+export const loadData = async (q, page) => {
+  if (!q)
+    throw new Erorr("Помилка: параметр 'q' не визначено або має значення null");
+  isLoading = true;
+
+  const url = queryString.stringifyUrl({
+    url: 'https://pixabay.com/api/',
+    query: {
+      q,
+      page,
+      key: '39347211-5080f64175fc5e961a62f5c8b',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: PER_PAGE,
+    },
+  });
+
+  try {
+    const { data } = await axios.get(url);
+
+    total += data.hits.length;
+    if (data.totalHits === 0) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    return data;
+  } catch (erorr) {
+    Notiflix.Notify.failure('Error');
+    console.log(erorr);
+    return null;
+  } finally {
+    isLoading = false;
+  }
+};
